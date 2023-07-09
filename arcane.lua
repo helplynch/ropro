7bbecd88187ad7ccd72cec5acdc404b686792d44
helplynch <108618400+helplynch@users.noreply.github.com>
2023-07-09 14:00:19 -0400

    Update arcane.lua

--- arcane.lua
+++ arcane.lua
@@ -1,4 +1,12 @@
--- Arcane was created by SlimShady
+--[[
+
+		SlimShady makes the weirdest but best scripts
+		Stay mad losers
+
+]]
+
+
+-- Instances
 
 local Main = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
@@ -13,6 +21,12 @@ local EspBtn = Instance.new("TextButton")
 local XrayBtn = Instance.new("TextButton")
 local TextLabel_2 = Instance.new("TextLabel")
 local OwlBtn = Instance.new("TextButton")
+local PlayerFrame = Instance.new("Frame")
+local UICorner_3 = Instance.new("UICorner")
+local MJumpBtn = Instance.new("TextButton")
+local TextLabel_3 = Instance.new("TextLabel")
+local HeadlessBtn = Instance.new("TextButton")
+local TextLabel_4 = Instance.new("TextLabel")
 
 -- Properties
 
@@ -145,42 +159,117 @@ OwlBtn.TextScaled = true
 OwlBtn.TextSize = 14
 OwlBtn.TextWrapped = true
 
+PlayerFrame.Name = "PlayerFrame"
+PlayerFrame.Parent = HackGui
+PlayerFrame.BackgroundColor3 = Color3.new(0.8, 0.8, 0.8)
+PlayerFrame.BorderColor3 = Color3.new(0, 0, 0)
+PlayerFrame.BorderSizePixel = 0
+PlayerFrame.Position = UDim2.new(1.01999998, 0, 0.605000019, 0)
+PlayerFrame.Size = UDim2.new(0, 89, 0, 97)
+PlayerFrame.Visible = false
+
+UICorner_3.Parent = PlayerFrame
+
+MJumpBtn.Name = "MJumpBtn"
+MJumpBtn.Parent = PlayerFrame
+MJumpBtn.BackgroundColor3 = Color3.new(0.313726, 0.54902, 0.6)
+MJumpBtn.BorderColor3 = Color3.new(0, 0, 0)
+MJumpBtn.BorderSizePixel = 0
+MJumpBtn.Position = UDim2.new(-1.7144707e-07, 0, 0.268041223, 0)
+MJumpBtn.Size = UDim2.new(0, 89, 0, 20)
+MJumpBtn.Font = Enum.Font.SourceSans
+MJumpBtn.Text = "Fly"
+MJumpBtn.TextColor3 = Color3.new(0.764706, 1, 0.572549)
+MJumpBtn.TextScaled = true
+MJumpBtn.TextSize = 14
+MJumpBtn.TextWrapped = true
+
+TextLabel_3.Parent = PlayerFrame
+TextLabel_3.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel_3.BackgroundTransparency = 1
+TextLabel_3.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(-1.7144707e-07, 0, 0, 0)
+TextLabel_3.Size = UDim2.new(0, 89, 0, 26)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "Player"
+TextLabel_3.TextColor3 = Color3.new(0, 0, 0)
+TextLabel_3.TextSize = 14
+
+HeadlessBtn.Name = "HeadlessBtn"
+HeadlessBtn.Parent = PlayerFrame
+HeadlessBtn.BackgroundColor3 = Color3.new(0.313726, 0.54902, 0.6)
+HeadlessBtn.BorderColor3 = Color3.new(0, 0, 0)
+HeadlessBtn.BorderSizePixel = 0
+HeadlessBtn.Position = UDim2.new(-1.7144707e-07, 0, 0.474226773, 0)
+HeadlessBtn.Size = UDim2.new(0, 89, 0, 20)
+HeadlessBtn.Font = Enum.Font.SourceSans
+HeadlessBtn.Text = "Headless"
+HeadlessBtn.TextColor3 = Color3.new(0.764706, 1, 0.572549)
+HeadlessBtn.TextScaled = true
+HeadlessBtn.TextSize = 14
+HeadlessBtn.TextWrapped = true
+
+TextLabel_4.Parent = HackGui
+TextLabel_4.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel_4.BackgroundTransparency = 1
+TextLabel_4.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(0, 0, 0.814516127, 0)
+TextLabel_4.Size = UDim2.new(0, 146, 0, 23)
+TextLabel_4.Font = Enum.Font.Gotham
+TextLabel_4.Text = "slimshadyplays"
+TextLabel_4.TextColor3 = Color3.new(0.631373, 1, 0.403922)
+TextLabel_4.TextScaled = true
+TextLabel_4.TextSize = 12
+TextLabel_4.TextWrapped = true
+
 -- Scripts
 
-print("Arcane has been loaded!")
-print("[Arcane] " .. game.Players.LocalPlayer.DisplayName .. " has loaded ArcaneHub")
-local function WOPF_fake_script() -- MiscBtn.LocalScript 
+function notif(txt)
+	local StarterGui = game:GetService("StarterGui")
+	StarterGui:SetCore("SendNotification",{
+		Title = "Arcane",
+		Text = txt,
+		Duration = 4
+	})
+end
+notif("Thanks for using Arcane!");
+notif("Currently logged in as " .. game.Players.LocalPlayer.DisplayName);
+
+local function BPVIXI_fake_script() -- MiscBtn.LocalScript 
 	local script = Instance.new('LocalScript', MiscBtn)
 
 	local miscmenu = script.Parent.Parent.MiscFrame
+	local playermenu = script.Parent.Parent.PlayerFrame
 	
 	script.Parent.MouseButton1Down:Connect(function()
 		if miscmenu.Visible == false then
 			miscmenu.Visible = true
+			playermenu.Visible = false
 		else
 			miscmenu.Visible = false
 		end
 	end)
 end
-coroutine.wrap(WOPF_fake_script)()
-local function JSBR_fake_script() -- PlayerBtn.LocalScript 
+coroutine.wrap(BPVIXI_fake_script)()
+local function RDKLZ_fake_script() -- PlayerBtn.LocalScript 
 	local script = Instance.new('LocalScript', PlayerBtn)
 
-	function notification(txt)
-		local StarterGui = game:GetService("StarterGui")
-		StarterGui:SetCore("SendNotification",{
-			Title = "Arcane",
-			Text = txt,
-			Duration = 4
-		})
-	end
+	local miscmenu = script.Parent.Parent.MiscFrame
+	local playermenu = script.Parent.Parent.PlayerFrame
 	
 	script.Parent.MouseButton1Down:Connect(function()
-		notification("This feature is coming soon to Arcane.");
+		if miscmenu.Visible == false then
+			playermenu.Visible = true
+			miscmenu.Visible = false
+		else
+			playermenu.Visible = false
+		end
 	end)
 end
-coroutine.wrap(JSBR_fake_script)()
-local function EPZYZX_fake_script() -- EspBtn.LocalScript 
+coroutine.wrap(RDKLZ_fake_script)()
+local function IYURLY_fake_script() -- EspBtn.LocalScript 
 	local script = Instance.new('LocalScript', EspBtn)
 
 	-- ESP MADE BY SH4DYY FOR ARCANE
@@ -678,60 +767,10 @@ local function EPZYZX_fake_script() -- EspBtn.LocalScript
 	
 	
 end
-coroutine.wrap(EPZYZX_fake_script)()
-local function AAKAW_fake_script() -- XrayBtn.LocalScript 
+coroutine.wrap(IYURLY_fake_script)()
+local function UUEFTW_fake_script() -- XrayBtn.LocalScript 
 	local script = Instance.new('LocalScript', XrayBtn)
 
-	-- XRAY MADE BY SH4DYY FOR ARCANE
-	
-	local Plrs = game:GetService('Players')
-	local MyPlr = Plrs.LocalPlayer
-	local xrayOn = false
-	
-	script.Parent.MouseButton1Down:Connect(function()
-		
-		
-		local xrayHotkey = ""
-	
-		local uis = game:GetService("UserInputService")
-	
-	
-		if xrayOn == false then
-			xrayOn = true
-		else 
-			xrayOn = false
-		end
-	
-	
-		uis.InputBegan:Connect(function(inp, processed)
-	
-	
-			if processed then return end
-	
-				for i, descendant in pairs(workspace:GetDescendants()) do
-	
-					if descendant:IsA("BasePart") then
-	
-						if xrayOn then
-	
-							if not descendant:FindFirstChild("OriginalTransparency") then
-	
-								local originalTransparency = Instance.new("NumberValue")
-								originalTransparency.Name = "OriginalTransparency"
-								originalTransparency.Value = descendant.Transparency
-								originalTransparency.Parent = descendant
-							end
-	
-							descendant.Transparency = 0.5
-	
-						else
-							descendant.Transparency = descendant.OriginalTransparency.Value
-						end
-					end
-				end
-		end)
-	end)
-	
 	function notification(txt)
 		local StarterGui = game:GetService("StarterGui")
 		StarterGui:SetCore("SendNotification",{
@@ -740,11 +779,12 @@ local function AAKAW_fake_script() -- XrayBtn.LocalScript
 			Duration = 4
 		})
 	end
-	
-	
+	script.Parent.MouseButton1Down:Connect(function()
+		notification("This feature is currently disabled by the developers.")
+	end)
 end
-coroutine.wrap(AAKAW_fake_script)()
-local function GNPMF_fake_script() -- OwlBtn.LocalScript 
+coroutine.wrap(UUEFTW_fake_script)()
+local function JZHKTCV_fake_script() -- OwlBtn.LocalScript 
 	local script = Instance.new('LocalScript', OwlBtn)
 
 	script.Parent.MouseButton1Down:Connect(function()
@@ -763,4 +803,131 @@ local function GNPMF_fake_script() -- OwlBtn.LocalScript
 	
 	
 end
-coroutine.wrap(GNPMF_fake_script)()
+coroutine.wrap(JZHKTCV_fake_script)()
+local function RUDXAG_fake_script() -- MJumpBtn.LocalScript 
+	local script = Instance.new('LocalScript', MJumpBtn)
+
+	-- MULTIJUMP BY SHADY TOO <3
+	
+	local flying = false
+	
+	script.Parent.MouseButton1Down:Connect(function()
+		repeat wait() 
+		until game.Players.LocalPlayer and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:findFirstChild("Head") and game.Players.LocalPlayer.Character:findFirstChild("Humanoid") 
+		local mouse = game.Players.LocalPlayer:GetMouse() 
+		repeat wait() until mouse
+		local plr = game.Players.LocalPlayer 
+		local torso = plr.Character.Head 
+		local flying = false
+		local deb = true 
+		local ctrl = {f = 0, b = 0, l = 0, r = 0} 
+		local lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+		local maxspeed = 400 
+		local speed = 5000 
+	
+		function Fly() 
+			local bg = Instance.new("BodyGyro", torso) 
+			bg.P = 9e4 
+			bg.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
+			bg.cframe = torso.CFrame 
+			local bv = Instance.new("BodyVelocity", torso) 
+			bv.velocity = Vector3.new(0,0.1,0) 
+			bv.maxForce = Vector3.new(9e9, 9e9, 9e9) 
+			repeat wait() 
+				plr.Character.Humanoid.PlatformStand = true 
+				if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then 
+					speed = speed+.5+(speed/maxspeed) 
+					if speed > maxspeed then 
+						speed = maxspeed 
+					end 
+				elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then 
+					speed = speed-1 
+					if speed < 0 then 
+						speed = 0 
+					end 
+				end 
+				if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
+					bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+					lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r} 
+				elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then 
+					bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+				else 
+					bv.velocity = Vector3.new(0,0.1,0) 
+				end 
+				bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0) 
+			until not flying 
+			ctrl = {f = 0, b = 0, l = 0, r = 0} 
+			lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+			speed = 0 
+			bg:Destroy() 
+			bv:Destroy() 
+			plr.Character.Humanoid.PlatformStand = false 
+		end 
+		mouse.KeyDown:connect(function(key)  
+			if flying then flying = false 
+			else 
+				flying = true 
+				Fly() 
+			end 
+			if key:lower() == "w" then 
+				ctrl.f = 1 
+			elseif key:lower() == "s" then 
+				ctrl.b = -1 
+			elseif key:lower() == "a" then 
+				ctrl.l = -1 
+			elseif key:lower() == "d" then 
+				ctrl.r = 1 
+			end 
+		end)
+		mouse.KeyUp:connect(function(key) 
+			if key:lower() == "w" then 
+				ctrl.f = 0 
+			elseif key:lower() == "s" then 
+				ctrl.b = 0 
+			elseif key:lower() == "a" then 
+				ctrl.l = 0 
+			elseif key:lower() == "d" then 
+				ctrl.r = 0 
+			end 
+		end)
+		
+		if flying == false then
+			flying = true
+		else
+			flying = false
+		end
+	end)
+	
+end
+coroutine.wrap(RUDXAG_fake_script)()
+local function UFRYXA_fake_script() -- HeadlessBtn.LocalScript 
+	local script = Instance.new('LocalScript', HeadlessBtn)
+
+	local isHeadless = false
+	local player = game.Players.LocalPlayer
+	
+	script.Parent.MouseButton1Down:Connect(function()
+		if isHeadless == false then
+			player.Character.Head.Transparency = 1
+			player.Character.Head.face.Transparency = 1
+			isHeadless = true
+		else
+			player.Character.Head.Transparency = 0
+			player.Character.Head.face.Transparency = 0
+			isHeadless = false
+		end
+	end)
+end
+coroutine.wrap(UFRYXA_fake_script)()
+local function TNXWEY_fake_script() -- TextLabel_4.LocalScript 
+	local script = Instance.new('LocalScript', TextLabel_4)
+
+	local isPremium = script.Parent.Parent.IsPremium.Value
+	local plr = game.Players.LocalPlayer
+	if plr.Name == "lp0nda" then
+		script.Parent.Text = game.Players.LocalPlayer.Name .. " [👑]"
+	else
+		script.Parent.Text = game.Players.LocalPlayer.Name .. " [-]"
+	end
+end
+coroutine.wrap(TNXWEY_fake_script)()
